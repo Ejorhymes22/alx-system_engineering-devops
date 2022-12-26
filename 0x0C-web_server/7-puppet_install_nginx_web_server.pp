fd3ea2505c091c62f298d81b71a3a3c674f6a56e
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-12-26 07:59:02 -0800

    7-puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,9 +3,9 @@
 include nginx
 
 
-file { '/var/www/html/index.html':
-content => 'Hello World!'
-}
+#file { '/var/www/html/index.html':
+#content => 'Hello World!'
+#}
 
-exec { "sed -i '50 i rewrite ^/redirect_me/$ 301 permanent;' /etc/nginx/sites-available/default'":
-}
+#exec { "sed -i '50 i rewrite ^/redirect_me/$ 301 permanent;' /etc/nginx/sites-available/default'":
+#}
