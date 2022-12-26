bcd3a110d03861a754bdbe9dbf8d386f6fcd0d50
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-12-26 01:11:31 -0800

    create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #this creates a file
 file { '/tmp/school':
 ensure  => 'present',
-owner   => 'www-data'
-group   => 'www-data'
-mode    => '0744'
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0744',
 content => 'I love Puppet'
 }
