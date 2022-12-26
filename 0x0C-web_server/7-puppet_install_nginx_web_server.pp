e0beecb8422db77e935be46c5a351674e4071eea
Lukmnan Mohammed <ejorhymes22@gmail.com>
2022-12-26 08:33:16 -0800

    p

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -17,9 +17,6 @@
 #content => 'Hello World!'
 #}
 
-#exec { "sed -i '50 i rewrite ^/redirect_me/$ 301 permanent;' /etc/nginx/sites-available/default'":
-#}
-
 
 exec { 'apt-get update':
   path => ['/bin', '/usr/bin'],
@@ -34,20 +31,27 @@ file { '/var/www/html/index.html':
   content => "Hello World!"
 }
 
-file { '/etc/nginx/sites-enabled/default':
-  content =>"
-server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-    root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
-    server_name _;
-    rewrite ^/redirect_me / permanent;
-	location / {
-		try_files \$uri \$uri/ =404;
-	}
-}"
+
+exec { "sed -i '50 i rewrite ^/redirect_me/$ 301 permanent;' /etc/nginx/sites-available/default'":
+  path => ['/bin', '/usr/bin'],
 }
+
+
+
+#file { '/etc/nginx/sites-enabled/default':
+ # content =>"
+#server {
+#	listen 80 default_server;
+#	listen [::]:80 default_server;
+ #   root /var/www/html;
+#	index index.html index.htm index.nginx-debian.html;
+ #   server_name _;
+#    rewrite ^/redirect_me / permanent;
+#	location / {
+#		try_files \$uri \$uri/ =404;
+#	}
+#}"
+#}
 
 exec { 'nginx':
   path    => ['/usr/sbin'],
